376babfe89ec228d2d0a0d6d9629f605653f60f5
Chin Yun Yu <ya70201@gmail.com>
2021-10-25 22:30:10 +0800

    feat: use torchaudio with sox backend to load wav

--- dataset.py
+++ dataset.py
@@ -1,9 +1,9 @@
 import os
 import numpy as np
-import soundfile as sf
 import random
 from torch.utils.data import Dataset
 from tqdm import tqdm
+import torchaudio
 
 
 class RandomWAVDataset(Dataset):
@@ -25,30 +25,19 @@ class RandomWAVDataset(Dataset):
 
         file_lengths = []
 
-        def get_nframes(info_str):
-            try:
-                return int(f_obj.extra_info.split('frames  : ')[1].split('\n')[0])
-            except:
-                byte_sec = int(info_str.split(
-                    'Bytes/sec     : ')[1].split('\n')[0])
-                data = int(info_str.split('data : ')[1].split('\n')[0])
-                sr = int(info_str.split('Sample Rate   : ')[1].split('\n')[0])
-                return int(data / byte_sec * sr)
-
         print("Gathering training files ...")
         for f in tqdm(os.listdir(self.data_path)):
             if f.endswith('.wav'):
                 filename = os.path.join(self.data_path, f)
-                f_obj = sf.SoundFile(filename)
+                meta = torchaudio.info(filename)
                 self.files.append(filename)
-                file_lengths.append(
-                    max(1, get_nframes(f_obj.extra_info) - segment + 1))
+                file_lengths.append(max(1, meta.num_frames - segment + 1))
 
                 if not self.sr:
-                    self.sr = f_obj.samplerate
+                    self.sr = meta.sample_rate
                 else:
-                    assert f_obj.samplerate == self.sr
-                f_obj.close()
+                    assert meta.sample_rate == self.sr
+
         self.file_lengths = np.array(file_lengths)
         self.boundaries = np.cumsum(
             np.array([0] + file_lengths)) / self.file_lengths.sum()
@@ -62,6 +51,7 @@ class RandomWAVDataset(Dataset):
         f, length = self.files[index], self.file_lengths[index]
         pos = int(round(length * (rand_pos - self.boundaries[index]) / (
             self.boundaries[index+1] - self.boundaries[index])))
-        x = sf.read(f, self.segment, start=pos, dtype='float32',
-                    always_2d=True, fill_value=0.)[0].mean(1)
+        x = torchaudio.load(f, frame_offset=pos,
+                            num_frames=self.segment)[0].mean(0)
+
         return x
