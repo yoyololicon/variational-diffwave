dd74deb1b28af41627b5e622ff1bf6024437156d
Chin Yun Yu <ya70201@gmail.com>
2022-02-11 17:10:47 +0800

    feat: remove old inference, add ddim

--- inference.py
+++ inference.py
@@ -5,40 +5,7 @@ from tqdm import tqdm
 from utils.utils import gamma2snr, snr2as, gamma2as, gamma2logas
 
 
-def reverse_process(z_1, mels, gamma, steps, model, with_amp=False):
-    assert gamma.size() == steps.size()
-    snr = gamma2snr(gamma)
-    alpha, var = snr2as(snr)
-    var_ts = - \
-        torch.expm1(F.softplus(gamma[:-1]) - F.softplus(gamma[1:]))
-    var_ts.relu_()
-
-    T = gamma.numel() - 1
-    z_t = z_1
-    for t in tqdm(range(T, 0, -1)):
-        s = t - 1
-        with amp.autocast(enabled=with_amp):
-            noise_hat = model(z_t, mels, steps[t:t+1])
-        noise_hat = noise_hat.float()
-        alpha_ts = alpha[t] / alpha[s]
-
-        # noise = (z_t - alpha[t] * eval_x) * var[t].rsqrt()
-        # kld += 0.5 * torch.expm1(gamma[t] - gamma[s]) * \
-        # F.mse_loss(noise_hat, noise)
-        # kld += 0.5 * (gamma[t] - gamma[s]) * \
-        #     F.mse_loss(noise_hat, noise)
-
-        mu = (z_t - var_ts[s] * var[t].rsqrt()
-              * noise_hat) / alpha_ts
-        z_t = mu
-        if s:
-            z_t += (var_ts[s] * var[s] / var[t]).sqrt() * \
-                torch.randn_like(z_t)
-
-    return z_t
-
-
-def reverse_process_new(z_1, mels, gamma, steps, model, with_amp=False):
+def reverse_process_new(z_1, mels, gamma, steps, model, with_amp=False, deterministic=False):
     log_alpha, log_var = gamma2logas(gamma)
     var = log_var.exp()
     alpha_st = torch.exp(log_alpha[:-1] - log_alpha[1:])
@@ -54,7 +21,25 @@ def reverse_process_new(z_1, mels, gamma, steps, model, with_amp=False):
         noise_hat = noise_hat.float()
         mu = (z_t - var[t].sqrt() * c[s] * noise_hat) * alpha_st[s]
         z_t = mu
-        if s:
+        if s and not deterministic:
             z_t += (var[s] * c[s]).sqrt() * torch.randn_like(z_t)
+
+    return z_t
+
+
+def reverse_process_ddim(z_1, mels, gamma, steps, model, with_amp=False):
+    Pm1 = torch.expm1(gamma[:-1] - gamma[1:])
+    log_alpha, log_var = gamma2logas(gamma)
+    alpha_st = torch.exp(log_alpha[:-1] - log_alpha[1:])
+    std = log_var.mul(0.5).exp()
+
+    T = gamma.numel() - 1
+    z_t = z_1
+    for t in tqdm(range(T, 0, -1)):
+        s = t - 1
+        with amp.autocast(enabled=with_amp):
+            noise_hat = model(z_t, mels, steps[t:t+1])
+        noise_hat = noise_hat.float()
+        z_t.add_(Pm1[s] * std[s] * noise_hat).mul_(alpha_st[s])
 
     return z_t
